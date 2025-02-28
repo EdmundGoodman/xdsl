5d1d4e8a468c95f1b0e79a89bf4b27cb2d89d682
EdmundGoodman <egoodman3141@gmail.com>
2025-02-28 15:51:46 +0000

    Fix rewriting to not use pre-rewritten version

--- benchmarks/rewriting.py
+++ benchmarks/rewriting.py
@@ -11,48 +11,27 @@ CTX = get_context()
 CTX.load_dialect(Arith)
 CTX.load_dialect(Builtin)
 
+CANONICALIZE_PASS = CanonicalizePass()
+
 
 class PatternRewrite:
     """Benchmark rewriting in xDSL."""
 
-    # WORKLOAD_CONSTANT_20 = parse_module(CTX, WorkloadBuilder.constant_folding(20))
+    WORKLOAD_CONSTANT_20 = parse_module(CTX, WorkloadBuilder.constant_folding(20))
     WORKLOAD_CONSTANT_100 = parse_module(CTX, WorkloadBuilder.constant_folding(100))
-    # WORKLOAD_CONSTANT_500 = parse_module(CTX, WorkloadBuilder.constant_folding(500))
-    WORKLOAD_CONSTANT_1000 = parse_module(CTX, WorkloadBuilder.constant_folding(1000))
-    # WORKLOAD_CONSTANT_2000 = parse_module(CTX, WorkloadBuilder.constant_folding(2000))
-    # WORKLOAD_CONSTANT_10000 = parse_module(CTX, WorkloadBuilder.constant_folding(10000))
-    # WORKLOAD_CONSTANT_100000 = parse_module(CTX, WorkloadBuilder.constant_folding(100000))
+    WORKLOAD_CONSTANT_1000 = parse_module(CTX, WorkloadBuilder.constant_folding(1_000))
 
-    # TODO: Does this cost dominate?
-    # CANONICALIZE_PASS = CanonicalizePass()
-
-    # def time_constant_folding_20(self) -> None:
-    #     """Time canonicalizing constant folding."""
-    #     CanonicalizePass().apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_20)
+    def time_constant_folding_20(self) -> None:
+        """Time canonicalizing constant folding."""
+        CANONICALIZE_PASS.apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_20.clone())
 
     def time_constant_folding_100(self) -> None:
         """Time canonicalizing constant folding."""
-        CanonicalizePass().apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_100)
-
-    # def time_constant_folding_500(self) -> None:
-    #     """Time canonicalizing constant folding."""
-    #     CanonicalizePass().apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_500)
+        CANONICALIZE_PASS.apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_100.clone())
 
     def time_constant_folding_1000(self) -> None:
         """Time canonicalizing constant folding."""
-        CanonicalizePass().apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_1000)
-
-    # def time_constant_folding_2000(self) -> None:
-    #     """Time canonicalizing constant folding."""
-    #     CanonicalizePass().apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_2000)
-
-    # def time_constant_folding_10000(self) -> None:
-    #     """Time canonicalizing constant folding."""
-    #     CanonicalizePass().apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_10000)
-
-    # def time_constant_folding_100000(self) -> None:
-    #     """Time canonicalizing constant folding."""
-    #     CanonicalizePass().apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_100000)
+        CANONICALIZE_PASS.apply(CTX, PatternRewrite.WORKLOAD_CONSTANT_1000.clone())
 
 
 if __name__ == "__main__":
@@ -62,12 +41,8 @@ if __name__ == "__main__":
 
     PATTERN_REWRITER = PatternRewrite()
     BENCHMARKS: dict[str, Callable[[], None]] = {
-        # "PatternRewriter.constant_folding_20": PATTERN_REWRITER.time_constant_folding_20,
+        "PatternRewriter.constant_folding_20": PATTERN_REWRITER.time_constant_folding_20,
         "PatternRewriter.constant_folding_100": PATTERN_REWRITER.time_constant_folding_100,
-        # "PatternRewriter.constant_folding_500": PATTERN_REWRITER.time_constant_folding_500,
         "PatternRewriter.constant_folding_1000": PATTERN_REWRITER.time_constant_folding_1000,
-        # "PatternRewriter.constant_folding_2000": PATTERN_REWRITER.time_constant_folding_2000,
-        # "PatternRewriter.constant_folding_10000": PATTERN_REWRITER.time_constant_folding_10000,
-        # "PatternRewriter.constant_folding_100000": PATTERN_REWRITER.time_constant_folding_100000,
     }
     profile(BENCHMARKS)
